23e37fb1d0dd224aa24dd45168e5f0c768d83a6f
OmarCerezo08 <cerezo0408@gmail.com>
2026-01-04 21:17:33 +0000

    Completed exercises

--- src/app.py
+++ src/app.py
@@ -1,4 +1,12 @@
 from utils import db_connect
 engine = db_connect()
 
-# your code here
+import streamlit as st
+import joblib
+
+@st.cache_resource  # Esto optimiza la carga en 2026
+def load_model():
+    return joblib.load("modelo_entrenado.pkl")
+
+model = load_model()
+
